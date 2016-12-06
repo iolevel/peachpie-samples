040fc2969d030f5fab767ae8b578df2721cbea22
Jakub Míšek <jmisek@gmail.com>
2016-12-06 10:28:29 +0100

    extension library sample

---
+++ extension-library/library/Program.cs
@@ -0,0 +1,25 @@
+﻿using Pchp.Core;
+
+[assembly: PhpExtension]
+
+// Example:
+public static class MyFunctions {
+    // declaration of a global PHP function 'mystrlen()'
+    public static int mystrlen(string str) { return (str != null) ? str.Length : -1; }
+
+    // declaration of a global PHP function 'myecho()' that requires a reference to current Pchp.Core.Context representing PHP runtime.
+    public static void myecho(Context ctx, int value) { ctx.Echo(value); }
+
+    // declaration of a global PHP constant 'MYCONST'
+    public const int MYCONST = 456;
+
+    // declaration of a global constant that is initialized dynamically
+    public static readonly string MYCONST2 = System.Environment.MachineName;
+}
+
+
+// declaration of PHP class
+public class SampleClass {
+    // declaration of method
+    public int foo(){ return 123; }
+}
---
+++ extension-library/samplephp/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function demo()
+{
+    // Example usage in PHP:
+    myecho(mystrlen("Hello PHP"));
+    echo MYCONST;
+    echo MYCONST2;
+    echo (new SampleClass)->foo();
+}
+
+demo();
