96a9a7bd97836176e34343e208f9ca0365ca3ac3
Jakub Míšek <jmisek@gmail.com>
2018-07-20 21:30:22 +0200

    updated deps
    - Peachpie.AspNetCore.Web

--- web-application/app/Program.cs
+++ web-application/app/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using Peachpie.AspNetCore.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +22,40 @@ namespace peachserver
                 .Build();
 
             host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // sample usage of URL rewrite:
+            var options = new RewriteOptions()
+                .AddRewrite(@"^rule/(\w+)", "index.php?word=$1", skipRemainingRules: true);
+
+            app.UseRewriter(options);
+
+            // enable session:
+            app.UseSession();
+
+            // enable .php files from compiled assembly:
+            app.UsePhp( new PhpRequestOptions() { ScriptAssembliesName = new[] { "website" } } );
+
+            //
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
