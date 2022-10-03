20856086edc49685be990ee279b5f398e05abbb4
Jakub Míšek <jmisek@gmail.com>
2022-10-03 13:29:28 +0200

    console sample updated to net6.0 and peachpie 1.1

--- console-application/main.php
+++ console-application/main.php
@@ -2,7 +2,7 @@
 
 function main() {
 
-    \System\Diagnostics\Debugger::Break();
+    // \System\Diagnostics\Debugger::Break();
 
     $content = json_decode(file_get_contents("content.json"));
     echo $content->message;
