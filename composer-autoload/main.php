ab4eeb3b68584c3383391e2114299127a8b7632c
Jakub Míšek <jmisek@gmail.com>
2024-07-07 17:36:27 +0200

    composer autoload sample

---
+++ composer-autoload/main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MyApp;
+
+function main() {
+
+    $x = new X;
+    $x->test();
+}
+
+main();
---
+++ composer-autoload/src/x.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyApp;
+
+class X {
+    function test() {
+        return __METHOD__;
+    }
+}
